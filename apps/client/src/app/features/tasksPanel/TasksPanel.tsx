9f0ec2f808420d18cb90b99c07cb18ae311e984d
Dmytro Beliasnyk <beliasnykdmytro@gmail.com>
2026-03-05 19:37:35 +0100

    refactor ux

--- apps/client/src/app/features/tasksPanel/TasksPanel.tsx
+++ apps/client/src/app/features/tasksPanel/TasksPanel.tsx
@@ -1,7 +1,5 @@
 import type { Task } from '@shared/types/task';
 import { memo } from 'react';
-import { useAppSelector } from '@store/redux';
-import { listSelectors } from '@store/slices/listSlice';
 import { ItemsManagementFormMode } from '@utils/itemsManagementFormOptions';
 import { TaskCard } from './TaskCard';
 import { Button } from '@ui/button/Button';
@@ -11,45 +9,31 @@ import { useTasks } from './hooks/useTasks';
 import { ScrollableList } from '@ui/scrollableList/ScrollableList';
 import { TasksPanelHeader } from './TasksPanelHeader';
 import { LuCirclePlus } from 'react-icons/lu';
-import clsx from 'clsx/lite';
+import type { List } from '@shared/types/list';
 
-export const TasksPanel = memo(() => {
-  const selectedList = useAppSelector(listSelectors.selectSelectedList);
+export const TasksPanel = memo(({ selectedList }: { selectedList: List }) => {
   const openForm = useOpenForm();
-  const tasks = useTasks(selectedList?.id);
+  const tasks = useTasks(selectedList.id);
 
   return (
     <>
-      <section
-        className={clsx(
-          'bg-secondary-bg flex flex-1 flex-col rounded-md p-4',
-          selectedList ? 'justify-between gap-2' : 'items-center justify-center text-center',
-        )}
-      >
-        {selectedList ? (
-          <>
-            <TasksPanelHeader selectedList={selectedList} />
-            <ScrollableList
-              items={tasks}
-              renderItem={(task: Task) => <TaskCard key={task.id} task={task} />}
-              emptyState={TASK_PANEL_TEXT.NO_TASKS}
-            />
-            <Button
-              className="flex w-fit gap-2 self-end"
-              onClick={() =>
-                openForm({ mode: ItemsManagementFormMode.AddTask, listId: selectedList.id })
-              }
-            >
-              <LuCirclePlus />
-              Add task
-            </Button>
-          </>
-        ) : (
-          <span className="text-muted-text inline-block w-3/4 text-4xl">
-            {TASK_PANEL_TEXT.NO_LIST_SELECTED}
-          </span>
-        )}
-      </section>
+      <div className="bg-secondary-bg flex flex-1 flex-col justify-between gap-2 rounded-md p-4">
+        <TasksPanelHeader selectedList={selectedList} />
+        <ScrollableList
+          items={tasks}
+          renderItem={(task: Task) => <TaskCard key={task.id} task={task} />}
+          emptyState={TASK_PANEL_TEXT.NO_TASKS}
+        />
+        <Button
+          className="flex w-fit gap-2 self-end"
+          onClick={() =>
+            openForm({ mode: ItemsManagementFormMode.AddTask, listId: selectedList.id })
+          }
+        >
+          <LuCirclePlus />
+          Add task
+        </Button>
+      </div>
     </>
   );
 });
--- apps/client/src/app/ui/Main/Main.tsx
+++ apps/client/src/app/ui/Main/Main.tsx
@@ -3,15 +3,16 @@ import { TasksPanel } from '@features/tasksPanel/TasksPanel';
 import { ItemsManagementForm } from '@features/forms/itemsManagement/ItemsManagementForm';
 import { useAppSelector } from '@store/redux';
 import { itemsManagementFormSelectors } from '@store/slices/formSlice';
+import { listSelectors } from '@store/slices/listSlice';
 
 export const Main = () => {
   const isFormOpen = useAppSelector(itemsManagementFormSelectors.isOpen);
+  const selectedList = useAppSelector(listSelectors.selectSelectedList);
 
   return (
     <>
-      <main className="flex gap-4 h-full overflow-hidden">
-        <ListsPanel />
-        <TasksPanel />
+      <main className="flex h-full gap-4 overflow-hidden">
+        {selectedList ? <TasksPanel selectedList={selectedList} /> : <ListsPanel />}
       </main>
       {isFormOpen && <ItemsManagementForm />}
     </>
