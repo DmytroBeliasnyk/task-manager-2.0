2ce50d0973ea93c6f69d1cabec301ced58b1f404
Dmytro Beliasnyk <beliasnykdmytro@gmail.com>
2026-03-26 19:43:51 +0100

    fix ui bug

--- apps/client/src/app/features/auth/AuthPage.tsx
+++ apps/client/src/app/features/auth/AuthPage.tsx
@@ -9,7 +9,7 @@ export const AuthPage = () => {
   const isFormSignUp = form === 'signup';
 
   return (
-    <div className="bg-gradient text-primary-text grid min-h-dvh grid-cols-1 grid-rows-[auto_auto_1fr] items-center justify-items-center gap-6 px-8 pt-4 text-base sm:grid-cols-2 sm:grid-rows-1 sm:gap-0 sm:p-0">
+    <div className="bg-gradient text-primary-text grid min-h-dvh grid-cols-1 grid-rows-[auto_auto_1fr] items-center justify-items-center gap-6 px-8 pt-4 text-base sm:grid-cols-2 sm:grid-rows-1 sm:justify-items-normal sm:gap-0 sm:p-0">
       <FormSwitcher isFormSignUp={isFormSignUp} setForm={setForm} />
       <PromoSection isFormSignUp={isFormSignUp} setForm={setForm} />
       <AuthForm isFormSignUp={isFormSignUp} />
